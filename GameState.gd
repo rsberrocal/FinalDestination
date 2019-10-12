9940807c9a5ec9c237036a70757ed5167a9c67d5
Okabe9 <sergixghost@gmail.com>
2019-10-12 07:27:13 +0200

    Mañana, con la idea del megaswitch se acaba logica del juego entera, solo hará falta sprites+animaciones, tal y como estaba previsto (la idea del megaswitch es la que se me ha ocurrido, si aparece una mas eficiente pues que se use esa.)(leer comentarios de GameState.gd)

--- GameState.gd
+++ GameState.gd
@@ -5,7 +5,14 @@ var situation
 func _ready():
 	pass # Replace with function body.
 func _decode(): 
-	
+	pass
+#Idea: hacer un megaswitch, cada uno perteneciente a una action de la situation, en casa switch se
+#llama  a la funcion que analiza el resultado a partir de action+tipoPlane.
+#Si por ejemplo hay 30 situations deberia haber un switch de 120 casos uno para cada animacion de accion+plane.
+#La animacion de la situacion deberia ir justo antes de estas dos por tanto iria por separado ya que
+#que es independiente de ambas. En el Json usaremos el nombre de la action para guardar ahí las
+#llaves del switch(o cualquier estructura mas eficiente(diccionario)).
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- Situations.gd
+++ Situations.gd
@@ -1,20 +1,27 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples
-var actions ={}
-
-
+var situations = {}
+var actions = {}
+var situationsAnim = {}
+var actionsAnim = {}
+var situation = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var file = File.new()
 	file.open("res://assets/config/situationConfig.json", file.READ)
 	var text = file.get_as_text()	
-	actions = JSON.parse(text).result
-	actions = actions['actions']
+	situations = JSON.parse(text).result
+	situations = situations['situations']
+	actions = actions['situations']['actions']
+	situationsAnim = situationsAnim['situation']['situationAnimation']
+	actionsAnim = actionsAnim['situations']['actionsAnimation']
 	file.close()
 	#To get random
-	var action = actions[randi() % actions.size()]	
-
+	situation = situations[randi() % situations.size()]	
+func getSituation():
+	return situation
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
